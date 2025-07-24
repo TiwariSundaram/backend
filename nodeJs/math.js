b7086a9a44f21f69768b8c4319cbaa4d0972929d
Sundaram Tiwari <145829895+TiwariSundaram@users.noreply.github.com>
2025-07-24 07:31:58 +0000

    a file from where a obj is export

---
+++ nodeJs/math.js
@@ -0,0 +1,17 @@
+const sum = (a,b)=>a+b;
+const mul = (a,b)=>a*b;
+const sub= (a,b)=>a-b;
+
+const pi = 3.14;
+const g = 9.8;
+
+const obj={
+
+    sum:sum,
+    mul:mul,
+    pi:pi,
+    sub:sub,
+    g:g
+}
+
+module.exports=obj;
